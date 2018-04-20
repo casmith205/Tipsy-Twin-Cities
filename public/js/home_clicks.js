a5c45c0310fd74f86444be1f9da97becfe34dd20
Carolyn Smith <casmith205@gmail.com>
2018-04-20 14:14:53 -0500

    user sign up adds to db

--- public/js/home_clicks.js
+++ public/js/home_clicks.js
@@ -1,13 +1,14 @@
 // Handle the clicks of the "Sign in" button 
 
 $(function() {
-    $("#userInput").on("click", function(event) {
+    $(".btn-signUp").on("click", function(event) {
+        event.preventDefault();
         // Grab info from the search...
         var user_name = $("#userName").val();
         var first_name = $("#firstName").val();
         var last_name = $("#lastName").val();
-        var email = $("#emails").val();
-        var password = $("#passWord").val();
+        var email = $("#emailAddressNew").val();
+        var password = $("#pwNew").val();
 
 
         // Set the information that we want to send to the API....
@@ -15,15 +16,17 @@ $(function() {
             user_name: user_name,
             first_name: first_name,
             last_name: last_name,
-            eamil: eamil,
+            email: email,
             password: password
         };
-        $.ajax("/api/user/search", {
+        console.log(userInfo);
+        $.ajax("/api/new_user", {
             type: "POST",
             data: userInfo
         }).then(
             function() {
                 console.log("Searched for the following: ", userInfo);
+                alert("You have successfully signed up!");
                 // Reload the page to get the updated list
                 location.reload();
             });
--- routes/user_api_routes.js
+++ routes/user_api_routes.js
@@ -1,35 +1,38 @@
 // Dependencies
 // =============================================================
-// Requiring our burger model
+// Requiring our model
 var db = require("../models");
 // Requiring express
 var express = require("express");
 var app = express.Router();
+
+
 // Routes
 // =============================================================
 module.exports = function (app) {
 
   // GET route for getting all of the users
-
   app.get("/api/all", function(req, res) {
-    User.findAll({}).then(function(results) {
+    db.User.findAll({}).then(function(results) {
       res.json(results);
     });
   });
+
   // POST route for saving a user
   app.post("/api/new_user", function(req, res) {
     console.log("User Data:");
     console.log(req.body);
-    User.create({
+    db.User.create({
       user_name: req.body.user_name,
       first_name: req.body.first_name,
       last_name: req.body.last_name,
-      email: req.body.email
+      email: req.body.email,
+      password: req.body.password
     });
   });
   // Get a specific user
   app.get("/api/:user", function(req, res) {
-    Book.findAll({
+    db.User.findAll({
       where: {
         user_name: req.User.user_name
       }
@@ -40,7 +43,7 @@ module.exports = function (app) {
   
   // PUT route for updating user
   app.put("/api/user/:id", function (req, res) {
-    db.Bur.update(req.body,
+    db.User.update(req.body,
       {
         where: {
           id: req.User.id
