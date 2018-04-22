d273972b97ac880a3b12c6d9b463f4cfc65894d3
Carolyn Smith <casmith205@gmail.com>
2018-04-22 08:11:26 -0500

    added on-click to post a deal to the db

--- public/js/add_clicks.js
+++ public/js/add_clicks.js
@@ -1,32 +1,37 @@
 // Handle the clicks of the "Add Restaurant" button and "Add Deal" button
 
 $(function() {
-    $(".btn-signUp").on("click", function(event) {
+    $("#submitBtnDeal").on("click", function(event) {
         event.preventDefault();
         // Grab info from the search...
-        var user_name = $("#userName").val();
-        var first_name = $("#firstName").val();
-        var last_name = $("#lastName").val();
-        var email = $("#emailAddressNew").val();
-        var password = $("#pwNew").val();
-
+        var restaurant_id = $("#restaurant").restId();
+        var dealDesc = $("#newDealDesc").val();
+        var dealDays= [];
+            // have to do a prop.("checked"), get a true/false, and push to an arr
+            // if in the checked arr, create a new deal
+        var currDealDay = "Monday";
+        var dealStart = $("#newDealStart").val();
+        var dealEnd = $("#newDealEnd").val();
+        var dealType = $("#newDealType").val();
+        var dealRestrictions = $("#newDealRestrictions").val();
 
         // Set the information that we want to send to the API....
-        var userInfo = {
-            user_name: user_name,
-            first_name: first_name,
-            last_name: last_name,
-            email: email,
-            password: password
+        var dealInfo = {
+            RestaurantId: restaurant_id,
+            deal_description: dealDesc,
+            day: currDealDay,
+            start_time: dealStart,
+            end_time: dealEnd,
+            restrictions: dealRestrictions,
+            deal_type: dealType
         };
         console.log(userInfo);
-        $.ajax("/api/new_user", {
+        $.ajax("/api/deals", {
             type: "POST",
-            data: userInfo
+            data: dealInfo
         }).then(
             function() {
-                console.log("Searched for the following: ", userInfo);
-                alert("You have successfully signed up!");
+                console.log("Added the following deal: ", dealInfo);
                 // Reload the page to get the updated list
                 location.reload();
             });
